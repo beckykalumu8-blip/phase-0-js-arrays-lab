d93f97b9eec9d8b3668cdd7cf610a7c0b4b43b3b
beckykalumu <beckykalumu8@gmail.com>
2025-11-20 11:56:32 +0300

    Arrays

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,21 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+const logFirstProduct = () => {
+  console.log(products[0]);
+}
+const addProduct = (Earphones) => {
+  products.push(Earphones);
+}
+const updateProductName = (index, Smartphone) => {
+  if (index >= 0 && index < products.length) {
+    products[index] = Smartphone;
+  }
+}
+const removeLastProduct = (Monitor) => {
+  products.pop(Monitor);
+}
+
 
 
 
